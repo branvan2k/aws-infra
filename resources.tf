00241006d7c4c17d8788407b77c72b3b50cead48
Alba Ferri <alba.ferri@alba.ferri>
2025-01-27 13:36:11 +0100

    Remove Splunk install

--- resources.tf
+++ resources.tf
@@ -182,17 +182,17 @@ resource "aws_instance" "demoinstance" {
   }
   
   # Installing splunk on newly created instance
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update -y",
-      "sudo amazon-linux-extras install docker -y",
-      "sudo service docker start",
-      "sudo usermod -a -G docker ec2-user",
-      "sudo chkconfig docker on",
-      "sudo yum install -y git",
-      "sudo chmod 666 /var/run/docker.sock",
-      "docker pull dhruvin30/dhsoniweb:v1",
-      "docker run -d -p 80:80 dhruvin30/dhsoniweb:latest"   
-  ]
- }
+#  provisioner "remote-exec" {
+#    inline = [
+#      "sudo yum update -y",
+#      "sudo amazon-linux-extras install docker -y",
+#      "sudo service docker start",
+#      "sudo usermod -a -G docker ec2-user",
+#      "sudo chkconfig docker on",
+#      "sudo yum install -y git",
+#      "sudo chmod 666 /var/run/docker.sock",
+#      "docker pull dhruvin30/dhsoniweb:v1",
+#      "docker run -d -p 80:80 dhruvin30/dhsoniweb:latest"   
+#  ]
+# }
 }
